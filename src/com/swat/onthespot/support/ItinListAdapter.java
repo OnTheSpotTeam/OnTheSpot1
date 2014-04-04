f2c35b927802e6af5569f2eaba9be211dc1ffa77
pzhao1 <pzhao1@swarthmore.edu>
2014-04-04 19:11:13 -0400

    section

--- src/com/swat/onthespot/support/ItinListAdapter.java
+++ src/com/swat/onthespot/support/ItinListAdapter.java
@@ -2,6 +2,7 @@ package com.swat.onthespot.support;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,31 +25,40 @@ public class ItinListAdapter extends CursorAdapter {
 
 	public ItinListAdapter(Context context, Cursor cursor){
 		super(context, cursor, 0);
+		cursor.moveToFirst();
+		while (!cursor.isAfterLast()){
+			Log.d("Adapter", "name: " + cursor.getString(cursor.getColumnIndex(OTSDatabase.ITINS_KEY_NAME))
+					+ "section: " + cursor.getInt(cursor.getColumnIndex(OTSDatabase.USERS_ITINS_KEY_SECTION)) );
+			cursor.moveToNext();
+		}
+		cursor.moveToFirst();
 	}
 
 	@Override
 	public View newView(Context context, Cursor cursor, ViewGroup parent) {
+		Log.d("newView", "name: " + cursor.getString(cursor.getColumnIndex(OTSDatabase.ITINS_KEY_NAME))
+				+ "section: " + cursor.getInt(cursor.getColumnIndex(OTSDatabase.USERS_ITINS_KEY_SECTION)) );
+		
 		View rowView;
 		LayoutInflater inflater =  (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		ViewHolder holder = new ViewHolder();
 
 		int section = cursor.getInt(cursor.getColumnIndex(OTSDatabase.USERS_ITINS_KEY_SECTION));
+		//int section = OTSDatabase.SECTION_CURRENT_CONTENT;
 		holder.section = section;
 		
-		/*
 		if (section == OTSDatabase.SECTION_CURRENT || section == OTSDatabase.SECTION_PAST){
 			rowView=inflater.inflate(R.layout.list_item_itinsection, null, true);
 			holder.sectionName=(TextView) rowView.findViewById(R.id.itinlist_sectionname);
 		}
 		else{
-		*/
 			rowView = inflater.inflate(R.layout.list_item_itins,null,true);
 			holder.image = (ImageView) rowView.findViewById(R.id.itinlist_item_image);
 			holder.name = (TextView) rowView.findViewById(R.id.itinlist_item_name);
 			holder.date = (TextView) rowView.findViewById(R.id.itinlist_item_date);
 			holder.comment = (TextView) rowView.findViewById(R.id.itinlist_item_comment);
 		
-		//}
+		}
 		rowView.setTag(holder);
 		return rowView;
 	}
@@ -57,17 +67,13 @@ public class ItinListAdapter extends CursorAdapter {
 	public void bindView(View view, Context context, Cursor cursor) {
 		ViewHolder holder = (ViewHolder) view.getTag();
 
-		/*
-		switch(holder.section){
-		case OTSDatabase.SECTION_CURRENT:
+		if (holder.section == OTSDatabase.SECTION_CURRENT){
 			holder.sectionName.setText("Current Itinerary");
-			break;
-		case OTSDatabase.SECTION_PAST:
+		}
+		else if (holder.section == OTSDatabase.SECTION_PAST){
 			holder.sectionName.setText("Past Itinerary");
-			break;
-		case OTSDatabase.SECTION_CURRENT_CONTENT:
-		case OTSDatabase.SECTION_PAST_CONTENT:
-		*/
+		}
+		else{
 			String name = cursor.getString(cursor.getColumnIndex(OTSDatabase.ITINS_KEY_NAME));
 			String date = cursor.getString(cursor.getColumnIndex(OTSDatabase.ITINS_KEY_DATE));
 			String comment = cursor.getString(cursor.getColumnIndex(OTSDatabase.ITINS_KEY_COMMENT));
@@ -79,12 +85,8 @@ public class ItinListAdapter extends CursorAdapter {
 					cursor.getString(cursor.getColumnIndex(OTSDatabase.ITINS_KEY_IMAGE)), 
 					"drawable", context.getPackageName()));
 					*/
-		/*
-			break;
-		default:
-		}
-		*/
 
+		}
 	}
 
 }
